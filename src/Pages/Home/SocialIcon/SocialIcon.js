4c934ccd7e60264277f98b7b3a661aacc9f1a952
Nasirul-Islam <nasirulislam866@gmail.com>
2021-12-15 01:05:28 +0600

    socials icon add

--- src/Pages/Home/SocialIcon/SocialIcon.js
+++ src/Pages/Home/SocialIcon/SocialIcon.js
@@ -4,15 +4,38 @@ import girl from '../../../images/girl-in-the-pool.jpg';
 import breakfast from '../../../images/breakfast.jpg';
 import room from '../../../images/room.jpg';
 import housekeeper from '../../../images/housekeeper-cleaning.jpg';
+import facebook from '../../../images/icon/facebook.png'
+import instagram from '../../../images/icon/instagram.png'
+import youtube from '../../../images/icon/youtube.png'
+import twitter from '../../../images/icon/twitter.png'
 
 const SocialIcon = () => {
     return (
         <Box>
             <Container>
-                <Box sx={{ py: 5, display: 'flex', justifyContain: 'space-butween' }}>
-                    <Typography>SOCIALS</Typography>
-                    <Box>hello</Box>
-                </Box>
+                <Grid container spacing={1} sx={{ py: 5 }}>
+                    <Grid item xs={12} md={6} sx={{ textAlign: 'left' }}>
+                        <Typography variant="h2" gutterBottom component="div" sx={{ fontWeight: 600 }}>SOCIALS</Typography>
+                    </Grid>
+                    <Grid item xs={12} md={6} sx={{ textAlign: 'right' }}>
+                        <a href="facebook.com" style={{ marginLeft: '15px' }}>
+                            <img width={'50px'} height={'50px'}
+                                src={facebook} alt="fb" />
+                        </a>
+                        <a href="twitter.com" style={{ marginLeft: '15px' }}>
+                            <img width={'50px'} height={'50px'}
+                                src={twitter} alt="twitter" />
+                        </a>
+                        <a href="instagram.com" style={{ marginLeft: '15px' }}>
+                            <img width={'50px'} height={'50px'}
+                                src={instagram} alt="instagram" />
+                        </a>
+                        <a href="youtube.com" style={{ marginLeft: '15px' }}>
+                            <img width={'50px'} height={'50px'}
+                                src={youtube} alt="youtube" />
+                        </a>
+                    </Grid>
+                </Grid>
             </Container>
             <Grid container spacing={0} sx={{ mb: -1 }} style={{ width: '100%' }}>
                 <Grid item xs={12} md={6} lg={3}>
